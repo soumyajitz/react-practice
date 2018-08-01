54216663426fedead3a3bf2a9cb9569793cb7aca
Soumyajit Sarkar <soumyajitsarkar@soumyajits-mbp.usca.ibm.com>
2018-08-01 14:03:46 -0700

    feat(cors): added cors middleware

--- recipes/react-recipes/client/src/index.js
+++ recipes/react-recipes/client/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:4444/graphql'
+})
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+    , document.getElementById('root')
+);
+
