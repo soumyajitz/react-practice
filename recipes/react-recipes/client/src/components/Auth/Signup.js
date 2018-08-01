3cb11c8f26934d4c177969f0bafda24caa042025
Soumyajit Sarkar <soumyajitsarkar@soumyajits-mbp.usca.ibm.com>
2018-08-01 15:08:25 -0700

    feat(Signup): add Signup component

--- recipes/react-recipes/client/src/components/Auth/Signup.js
+++ recipes/react-recipes/client/src/components/Auth/Signup.js
@@ -3,7 +3,16 @@ import React from 'react';
 class Signup extends React.Component {
     render() {
         return (
-            <div>Sign up</div>
+            <div className="App">
+                <h2 className="App">Sign up</h2>
+                <form className="form">
+                    <input type="text" name="username" placeholder="Username" />
+                    <input type="email" name="email" placeholder="Email" />
+                    <input type="password" name="password" placeholder="Password" />
+                    <input type="password" name="passwordConfirmation" placeholder="Confirm Password" />
+                    <button type="submit" className="button-primary">Submit</button>
+                </form>
+            </div>
         )
     }
 }
